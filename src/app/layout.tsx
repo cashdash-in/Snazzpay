5c045a6e7b8d2e598b8c5bc91cb71830c68b738c
Christopher D'souza <christopher.p.dsouza@gmail.com>
2025-08-26 18:27:23 +0000

    why i cant see the full App

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type {Metadata} from 'next';
 import './globals.css';
 import { Inter } from 'next/font/google';
@@ -23,7 +24,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
       </head>
-      <body className="font-body antialiased h-full bg-background">
+      <body className="font-body antialiased h-full">
         {children}
         <Toaster />
       </body>
