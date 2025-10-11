4bfe81a57a362ab364e3fcb877451a19437944d9
Christopher D'souza <christopher.p.dsouza@gmail.com>
2025-10-11 11:57:16 +0000

    No I am not delploying until we are sure you have corrected the issue Di

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,6 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
-// This file is somehow breaking the build.
-// Remove the genkit initialization for now.
-// export const ai = genkit({
-//   plugins: [googleAI()],
-//   model: 'gemini-1.5-flash',
-// });
-
+export const ai = genkit({
+  plugins: [googleAI()],
+});
