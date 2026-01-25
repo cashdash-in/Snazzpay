e3111fd70d2b95921a674df4bf35c52d32bfb162
Christopher D'souza <christopher.p.dsouza@gmail.com>
2026-01-25 12:42:28 +0000

    fix(config): correct next.config.js syntax to CommonJS

--- next.config.js
+++ next.config.js
@@ -1,22 +1,8 @@
-
-const { hostname } = new URL(process.env.SHOPIFY_STORE_URL);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.shopify.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname,
-        pathname: '/**',
-      },
-    ],
+  experimental: {
+    instrumentationHook: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
