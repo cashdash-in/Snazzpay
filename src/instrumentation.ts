6fe839ac9aaec3c1dfc9971b69f3dea2644303e9
Christopher D'souza <christopher.p.dsouza@gmail.com>
2026-01-25 13:28:17 +0000

    fix: correct genkit initialization

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,11 +1,11 @@
 export async function register() {
   // We only want to run this code on the server.
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    const { initializeGenkit } = await import('@genkit-ai/core');
+    const core = (await import('@genkit-ai/core')).default;
     // Use dynamic import to get the default export from the firebase plugin.
     const firebase = (await import('@genkit-ai/firebase')).default;
 
-    initializeGenkit({
+    core.initializeGenkit({
       plugins: [firebase()],
       logLevel: 'debug',
       enableTracingAndMetrics: true,
